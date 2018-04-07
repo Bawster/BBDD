a135bae5a466290a58e24955455cc47cff0b26e6
havatar11m <32900002+havatar11m@users.noreply.github.com>
2018-04-07 15:46:23 +0200

    Update base.sql

--- base.sql
+++ base.sql
@@ -1,7 +1,34 @@
-drop DATABASE IF EXISTS entornos;
+DROP DATABASE IF EXISTS entornos;
 CREATE DATABASE entornos;
-use entornos
-CREATE TABLE Persona(Nick VARCHAR(50), Nombre VARCHAR(59), Apellido VARCHAR(59), Email VARCHAR(50),
-Contraeña VARCHAR(50), Edad INT, PRIMARY KEY(Nick));
-CREATE TABLE Empresa(Nombre VARCHAR(59), VerificacionEmpresa VARCHAR(59), Ubicacion VARCHAR(50),
-Horario VARCHAR(50), Descripcion VARCHAR(70));
+USE entornos
+
+CREATE TABLE Persona(
+  Nick VARCHAR(50),
+  Nombre VARCHAR(50),
+  Email VARCHAR(50) UNIQUE,
+  Contraeña VARCHAR(50),
+  Edad TINYINT UNSIGNED,
+  Sexo ENUM('Hombre', 'Mujer', 'Otro'),
+  PRIMARY KEY(Nick));
+  
+CREATE TABLE Empresa(
+  Nombre VARCHAR(50),
+  Verificacion_Empresa VARCHAR(50),
+  Ubicacion VARCHAR(50),
+  Horario VARCHAR(50),
+  Descripcion TEXT,
+  PRIMARY KEY Verificacion_Empresa);
+
+CREATE TABLE Evento(
+  Nombre VARCHAR(20),
+  Tiempo_Inicio TIME,
+  Tiempo_Fin
+  TIME,
+  Ubicacion VARCHAR(20),
+  Descripcion TEXT,
+  PRIMARY KEY Nombre);
+
+CREATE TABLE
+CREATE TABLE
+CREATE TABLE
+CREATE TABLE
