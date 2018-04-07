44c7638bc53b1cdf97027284f412c3c3bd4953ac
havatar11m <32900002+havatar11m@users.noreply.github.com>
2018-04-07 16:00:19 +0200

    Update base.sql

--- base.sql
+++ base.sql
@@ -5,6 +5,7 @@ USE entornos
 CREATE TABLE Persona(
   Nick VARCHAR(50),
   Nombre VARCHAR(50),
+  Foto TEXT,
   Email VARCHAR(50) UNIQUE,
   Contraeña VARCHAR(50),
   Edad TINYINT UNSIGNED,
@@ -14,6 +15,7 @@ CREATE TABLE Persona(
 CREATE TABLE Empresa(
   Nombre VARCHAR(50),
   Verificacion_Empresa VARCHAR(50),
+  TIpo_Empresa ENUM('Bar', 'Pub', 'Restaurante', 'Recreativos'),
   Ubicacion VARCHAR(50),
   Horario VARCHAR(50),
   Descripcion TEXT,
@@ -21,9 +23,8 @@ CREATE TABLE Empresa(
 
 CREATE TABLE Evento(
   Nombre VARCHAR(20),
-  Tiempo_Inicio TIME,
-  Tiempo_Fin
-  TIME,
+  Tiempo_Inicio DATETIME,
+  Tiempo_Fin DATETIME,
   Ubicacion VARCHAR(20),
   Descripcion TEXT,
   PRIMARY KEY Nombre);
