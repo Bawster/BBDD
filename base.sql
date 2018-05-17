4148188200477aafc2d1dfad68c42caa5632cbcc
sergiosanzllamas <sanzsergio4ab@gmail.com>
2018-05-17 19:28:28 +0200

    acabda

--- base.sql
+++ base.sql
@@ -4,32 +4,28 @@ USE entornos
 
 CREATE TABLE Persona(
   Nick VARCHAR(50),
-  Nombre VARCHAR(50) NOT NULL,
+  Nombre VARCHAR(50),
   Foto TEXT,
-  Email VARCHAR(50) UNIQUE NOT NULL,
-  Contraeña VARCHAR(50) NOT NULL,
-  Edad TINYINT UNSIGNED NOT NULL,
-  Sexo ENUM('Hombre', 'Mujer', 'Otro') NOT NULL,
+  Email VARCHAR(50) UNIQUE,
+  Contraeña VARCHAR(50),
+  Edad TINYINT UNSIGNED,
+  Sexo ENUM('Hombre', 'Mujer', 'Otro'),
   PRIMARY KEY(Nick));
   
 CREATE TABLE Empresa(
-  Nombre VARCHAR(50) NOT NULL,
-  Verificacion_Empresa VARCHAR(50) NOT NULL,
-  TIpo_Empresa ENUM('Bar', 'Pub', 'Restaurante', 'Recreativos') NOT NULL,
-  Ubicacion VARCHAR(50) NOT NULL,
+  Nombre VARCHAR(50),
+  Verificacion_Empresa VARCHAR(50),
+  TIpo_Empresa ENUM('Bar', 'Pub', 'Restaurante', 'Recreativos'),
+  Ubicacion VARCHAR(50),
   Horario VARCHAR(50),
-  Descripcion TEXT NOT NULL,
-  PRIMARY KEY Verificacion_Empresa);
+  Descripcion TEXT,
+  PRIMARY KEY (Verificacion_Empresa));
 
 CREATE TABLE Evento(
-  Nombre VARCHAR(20) NOT NULL,
-  Tiempo_Inicio DATETIME NOT NULL,
-  Tiempo_Fin DATETIME NOT NULL,
-  Ubicacion VARCHAR(20) NOT NULL,
+  Nombre VARCHAR(20),
+  Tiempo_Inicio DATETIME,
+  Tiempo_Fin DATETIME,
+  Ubicacion VARCHAR(20),
   Descripcion TEXT,
-  PRIMARY KEY Nombre);
+  PRIMARY KEY (Nombre));
 
-CREATE TABLE
-CREATE TABLE
-CREATE TABLE
-CREATE TABLE
